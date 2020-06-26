ff54fc775bcd412632fae09f9022c6ae48470b8e
João Paulo Augustus <augustusdclxvi@gmail.com>
2020-06-26 11:07:29 -0300

    Commit 0.2

--- ProjetoMVC2020-master/BLL/tblClienteBLL.cs
+++ ProjetoMVC2020-master/BLL/tblClienteBLL.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data;
+using ProjetoMVC2020.DAL;
 
 namespace ProjetoMVC2020.BLL
 {
